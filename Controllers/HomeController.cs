4b0de886b83e0049a42d4c7d6ee8c82629e8359d
nguyentrongnguyen3004-eng <nguyentrongnguyen3004@gmail.com>
2025-12-01 18:44:14 +0700

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,13 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace QLTHUVIEN.Controllers
+namespace LTWeb01_Bai01.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            return View();
+        }
+
+        public ActionResult Buoi1()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult HienThi(string id)
+        {
+            return Content("ID = " +id);
+        }
+
+        public ActionResult Index3(string id, string name)
+        {
+            //Use ViewBag
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+
+            //Use ViewData
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
